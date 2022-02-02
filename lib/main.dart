30a1a4dc425134ee005ed13f349c4f42eff41446
txkidxki <rattanakorn_re@cmu.ac.th>
2022-02-02 22:07:35 +0700

    edit main page
    change text , icon

--- lib/main.dart
+++ lib/main.dart
@@ -42,15 +42,10 @@ class _MyHomePageState extends State<MyHomePage> {
                   onPressed: () {},
                 ),
                 Container(
-                    width: 125.0,
+                    width: 130.0,
                     child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      mainAxisAlignment: MainAxisAlignment.end,
                       children: <Widget>[
-                        // IconButton(
-                        //   icon: Icon(Icons.filter_list),
-                        //   color: Colors.white,
-                        //   onPressed: () {},
-                        // ),
                        IconButton(
                           onPressed: () {
                             showSearch(
@@ -59,7 +54,7 @@ class _MyHomePageState extends State<MyHomePage> {
                             );
                           },
                           icon: const Icon(Icons.search),
-                        ),
+                       )
                       ],
                     ))
               ],
@@ -70,14 +65,14 @@ class _MyHomePageState extends State<MyHomePage> {
             padding: EdgeInsets.only(left: 40.0),
             child: Row(
               children: <Widget>[
-                Text('Healthy',
+                Text('Food',
                     style: TextStyle(
                         fontFamily: 'Montserrat',
                         color: Colors.white,
                         fontWeight: FontWeight.bold,
                         fontSize: 25.0)),
                 SizedBox(width: 10.0),
-                Text('Food',
+                Text('Menu',
                     style: TextStyle(
                         fontFamily: 'Montserrat',
                         color: Colors.white,
@@ -113,51 +108,51 @@ class _MyHomePageState extends State<MyHomePage> {
                     Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: <Widget>[
-                    Container(
-                      height: 65.0,
-                      width: 60.0,
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                            color: Colors.grey,
-                            style: BorderStyle.solid,
-                            width: 1.0),
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      child: Center(
-                        child: Icon(Icons.search, color: Colors.black),
-                      ),
-                    ),
-                    Container(
-                      height: 65.0,
-                      width: 60.0,
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                            color: Colors.grey,
-                            style: BorderStyle.solid,
-                            width: 1.0),
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      child: Center(
-                        child: Icon(Icons.shopping_basket, color: Colors.black),
-                      ),
-                    ),
-                    Container(
-                      height: 65.0,
-                      width: 120.0,
-                      decoration: BoxDecoration(
-                          border: Border.all(
-                              color: Colors.grey,
-                              style: BorderStyle.solid,
-                              width: 1.0),
-                          borderRadius: BorderRadius.circular(10.0),
-                          color: Color(0xFF1C1428)),
-                      child: Center(
-                          child: Text('Checkout',
-                              style: TextStyle(
-                                  fontFamily: 'Montserrat',
-                                  color: Colors.white,
-                                  fontSize: 15.0))),
-                    )
+                    // Container(
+                    //   height: 65.0,
+                    //   width: 60.0,
+                    //   decoration: BoxDecoration(
+                    //     border: Border.all(
+                    //         color: Colors.grey,
+                    //         style: BorderStyle.solid,
+                    //         width: 1.0),
+                    //     borderRadius: BorderRadius.circular(10.0),
+                    //   ),
+                    //   child: Center(
+                    //     child: Icon(Icons.search, color: Colors.black),
+                    //   ),
+                    // ),
+                    // Container(
+                    //   height: 65.0,
+                    //   width: 60.0,
+                    //   decoration: BoxDecoration(
+                    //     border: Border.all(
+                    //         color: Colors.grey,
+                    //         style: BorderStyle.solid,
+                    //         width: 1.0),
+                    //     borderRadius: BorderRadius.circular(10.0),
+                    //   ),
+                    //   child: Center(
+                    //     child: Icon(Icons.shopping_basket, color: Colors.black),
+                    //   ),
+                    // ),
+                    // Container(
+                    //   height: 65.0,
+                    //   width: 120.0,
+                    //   decoration: BoxDecoration(
+                    //       border: Border.all(
+                    //           color: Colors.grey,
+                    //           style: BorderStyle.solid,
+                    //           width: 1.0),
+                    //       borderRadius: BorderRadius.circular(10.0),
+                    //       color: Color(0xFF1C1428)),
+                    //   child: Center(
+                    //       child: Text('Checkout',
+                    //           style: TextStyle(
+                    //               fontFamily: 'Montserrat',
+                    //               color: Colors.white,
+                    //               fontSize: 15.0))),
+                    // )
                   ],
                 )
               ],
@@ -218,7 +213,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 )
               ),
               IconButton(
-                icon: Icon(Icons.add),
+                icon: Icon(Icons.star_border),
                 color: Colors.black,
                 onPressed: () {}
               )
