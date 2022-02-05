ad031e1da8fd462bdc92b5ed50e9077b0270b33b
munlikaCh <munlika_ch@cmu.ac.th>
2022-02-05 23:24:19 +0700

    edit BGcolor

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: MyHomePage(),
+      theme: ThemeData(primarySwatch: Colors.amber),
       debugShowCheckedModeBanner: false,
     );
   }
@@ -30,26 +31,29 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color(0xFF21BFBD),
-        title: Text('Food Menu',
+        backgroundColor: Color(0xFFFFFFFF),
+        title: Text('EHE COOKING',
             style: TextStyle(
                 fontFamily: 'Montserrat',
-                color: Colors.white,
+                color: Colors.amber[800],
                 fontWeight: FontWeight.bold,
                 fontSize: 25.0)),
         actions: [
           IconButton(
+            color: Colors.amber[800],
             icon: Icon(Icons.home),
             onPressed: () {
             },
           ),
           IconButton(
+            color: Colors.amber[800],
             icon: Icon(Icons.list),
             onPressed: () {
               _pushSaved();
             },
           ),
           IconButton(
+            color: Colors.amber[800],
             onPressed: () {
               showSearch(
                 context: context,
@@ -60,81 +64,84 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ],
       ),
-      backgroundColor: Color(0xFF21BFBD),
-      body: ListView(
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.only(top: 15.0, left: 10.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Container(
-                    width: 130.0,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: <Widget>[
-                        // IconButton(
-                        //   icon: Icon(Icons.search_rounded),
-                        //   color: Colors.white,
-                        //   onPressed: () {},
-                        // ),
-                        // IconButton(
-                        //   icon: Icon(Icons.menu),
-                        //   color: Colors.white,
-                        //   onPressed: () {},
-                        // )
-                      ],
-                    ))
-              ],
-            ),
-          ),
-          SizedBox(height: 25.0),
-          Padding(
-            padding: EdgeInsets.only(left: 40.0),
-            child: Row(
-            ),
-          ),
-          SizedBox(height: 40.0),
-          Container(
-            height: MediaQuery.of(context).size.height - 185.0,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(topLeft: Radius.circular(75.0)),
-            ),
-            child: ListView(
-              primary: false,
-              padding: EdgeInsets.only(left: 25.0, right: 20.0),
-              children: <Widget>[
-                Padding(
-                    padding: EdgeInsets.only(top: 45.0),
-                    child: Container(
-                        height: MediaQuery.of(context).size.height - 300.0,
-                        child: FutureBuilder(
-                          builder: (context, snapshot) {
-                            var data = json.decode(snapshot.data.toString());
-                            return ListView.builder(
-                              itemBuilder: (BuildContext context, int index) {
-                                return _buildFoodItem(
-                                    data[index]['id'],
-                                    data[index]['imgPath'],
-                                    data[index]['name'],
-                                    data[index]['price']);
-                              },
-                              itemCount: data.length,
-                            );
-                          },
-                          future: DefaultAssetBundle.of(context)
-                              .loadString('assets/data.json'),
-                        ))),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                  ],
-                )
-              ],
-            ),
+      backgroundColor: Color(0xFFFFFFFF),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment(0.0, 1.0),
+            end: Alignment(0.0, -1.0),
+            colors: [
+              Theme.of(context).primaryColorLight,
+              Theme.of(context).primaryColorLight,
+              Theme.of(context).primaryColorDark,
+            ]
           )
-        ],
+        ),
+        child: ListView(
+          children: <Widget>[
+            
+            SizedBox(height: 25.0),
+            Padding(
+              padding: EdgeInsets.only(left: 40.0),
+              child: Row(
+                 children: <Widget>[
+                  Text('Food',
+                      style: TextStyle(
+                          fontFamily: 'Montserrat',
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 25.0)),
+                  SizedBox(width: 10.0),
+                  Text('Menu',
+                      style: TextStyle(
+                          fontFamily: 'Montserrat',
+                          color: Colors.white,
+                          fontSize: 25.0))
+                ],
+              ),
+            ),
+            SizedBox(height: 20.0),
+            Container(
+              height: MediaQuery.of(context).size.height - 180.0,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(topLeft: Radius.circular(75.0)),
+              ),
+              child: ListView(
+                primary: false,
+                padding: EdgeInsets.only(left: 25.0, right: 20.0),
+                children: <Widget>[
+                  Padding(
+                      padding: EdgeInsets.only(top: 45.0),
+                      child: Container(
+                          height: MediaQuery.of(context).size.height - 300.0,
+                          child: FutureBuilder(
+                            builder: (context, snapshot) {
+                              var data = json.decode(snapshot.data.toString());
+                              return ListView.builder(
+                                itemBuilder: (BuildContext context, int index) {
+                                  return _buildFoodItem(
+                                      data[index]['id'],
+                                      data[index]['imgPath'],
+                                      data[index]['name'],
+                                      data[index]['price']);
+                                },
+                                itemCount: data.length,
+                              );
+                            },
+                            future: DefaultAssetBundle.of(context)
+                                .loadString('assets/data.json'),
+                          ))),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
@@ -172,11 +179,6 @@ class _MyHomePageState extends State<MyHomePage> {
                           fontFamily: 'Montserrat',
                           fontSize: 17.0,
                           fontWeight: FontWeight.bold)),
-                  Text(price,
-                      style: TextStyle(
-                          fontFamily: 'Montserrat',
-                          fontSize: 15.0,
-                          color: Colors.grey))
                 ])
               ])),
               IconButton(
